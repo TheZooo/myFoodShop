017842fc7d8fde66d81ae9ecfc6d53d35cf94696
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2019-03-05 12:41:42 -1000

    Only with JSON and on server

--- foodShopFunc.js
+++ foodShopFunc.js
@@ -1,37 +1,5 @@
 var d = document;
-var myData = {
-  "food": [{
-      "item": "Soft Shell Crab",
-      "price": 4.99,
-      "desc": "A crunchy crab served with a lemon"
-    },
-    {
-      "item": "Lobster",
-      "price": 11.99,
-      "desc": "Throughly boiled lobster, along with our own dipping sauce"
-    },
-    {
-      "item": "Sashimi Platter",
-      "price": 14.99,
-      "desc": "Can have salmon, tuna, ahi, beef, squid, in addition to soy sauce"
-    },
-    {
-      "item": "Shark Fin Soup",
-      "price": 10.99,
-      "desc": "A traditional chinese soup made from simmering shark fins"
-    },
-    {
-      "item": "Calamari",
-      "price": 3.99,
-      "desc": "Fried squid, good for a appetizer meal"
-    },
-    {
-      "item": "Grilled Squid",
-      "price": 3.99,
-      "desc": "Squid grilled, seasoned, and with lemon juices"
-    }
-  ]
-};
+var myData;
 var boxes = d.getElementsByClassName('itemBox');
 var i = 0;
 var iv = 0;
@@ -48,23 +16,21 @@ var ordersItem = [];
 var boxChecker = [];
 
 function startA() {
-  /*
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) { //4 = ready ; 200 = okay
       myData = JSON.parse(this.responseText);
+      for (i; i < boxes.length; i++) {
+        d.getElementsByClassName('itemHead')[i].innerHTML = myData.food[i].item;
+        d.getElementsByClassName('itemDesc')[i].innerHTML = myData.food[i].desc;
+        d.getElementsByClassName('itemCost')[i].innerHTML = "$" + myData.food[i].price;
+        d.getElementsByClassName('amtOrder')[i].value = 0;
+        }
       }
     };
   xmlhttp.open("GET", "sample.json", true);
   xmlhttp.send();
-  */
   localStorage.clear();
-  for (i; i < boxes.length; i++) {
-    d.getElementsByClassName('itemHead')[i].innerHTML = myData.food[i].item;
-    d.getElementsByClassName('itemDesc')[i].innerHTML = myData.food[i].desc;
-    d.getElementsByClassName('itemCost')[i].innerHTML = "$" + myData.food[i].price;
-    d.getElementsByClassName('amtOrder')[i].value = 0;
-  }
   d.getElementById('taxNow').innerHTML = "Tax rate: " + tax + "%";
 }
 
@@ -278,7 +244,7 @@ function discarded() {
 
 function done() {
   localStorage.clear();
-  location.href = "myFoodShop.html";
+  location.href = "index.html";
 }
 
 function confirmTip() {
